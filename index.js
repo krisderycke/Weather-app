26dcfc85c7b39ec047b9f2df1c8420802f01a70d
Kris de Rycke <derycke.kris@gmail.com>
2019-07-01 11:13:41 +0200

    add little CSS styling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const key = "469c241d9a72addf7ad83b0a96ed7206";
 let city = document.getElementById("input");
-let link = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${key}`;
+let cityName = city.value;
+let link = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${key}`;
+// const axios = require("axios");
+console.log(key, cityName, link);
