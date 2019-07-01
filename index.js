73797f8b8b45cef9d0448e384b9e64333e21a944
Kris de Rycke <derycke.kris@gmail.com>
2019-07-01 14:53:20 +0200

    have the API show results in console

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const key = "469c241d9a72addf7ad83b0a96ed7206";
-let link = `http://api.openweathermap.org/data/2.5/forecast?q=${city},uk&APPID=${key}`;
-let city = document.getElementById("input");
 
-function getweather() {}
+function getWeather() {
+  let button = document.getElementById("search");
+  button.addEventListener("click", async function(e) {
+    let city = document.getElementById("input").value;
+
+    let link = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${key}`;
+    let response = await axios.get(link);
+    console.log(response.data);
+  });
+}
+getWeather();
