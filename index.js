08e5babf05ff9cfde6c453bbb053096badf8e3ed
Kris de Rycke <derycke.kris@gmail.com>
2019-07-01 14:25:52 +0200

    change layout en make container fulle responsive

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const key = "469c241d9a72addf7ad83b0a96ed7206";
+let link = `http://api.openweathermap.org/data/2.5/forecast?q=${city},uk&APPID=${key}`;
 let city = document.getElementById("input");
-let cityName = city.value;
-let link = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${key}`;
 
-//  const axios = require("axios");
+function getweather() {}
