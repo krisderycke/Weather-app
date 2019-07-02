1bf7a45ee6e66bffa62227fcff54d954b8cf3e2a
Kris de Rycke <derycke.kris@gmail.com>
2019-07-02 13:49:05 +0200

    make week and days array and push the responses into it

--- index.js
+++ index.js
@@ -6,27 +6,41 @@ function getWeather() {
     let city = document.getElementById("input").value;
     let link = `http://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&APPID=${key}&unit=metrics`;
     let response = await axios.get(link);
-    // let result = document.getElementById("result");
-    console.log(response.data);
-    console.log(response.data.list[0].main);
-    console.log(response.data.list[0].main.temp);
-    console.log(response.data.list[8].main.temp);
-    console.log(response.data.list[16].main.temp);
-    console.log(response.data.list[24].main.temp);
-    console.log(response.data.list[32].main.temp);
-    let weather = response.data.list[0].main;
+
+    // console.log(response.data);
+    // console.log(response.data.list[0].main);
+    // console.log(response.data.list[0].main.temp);
+    // console.log(response.data.list[8].main.temp);
+    // console.log(response.data.list[16].main.temp);
+    // console.log(response.data.list[24].main.temp);
+    // console.log(response.data.list[32].main.temp);
+    // let weather = response.data.list[0].main;
 
     // document.getElementById("current").innerHTML += weather.temp + " °C";
     // document.getElementById("min").innerHTML += weather.temp_min + " °C";
     // document.getElementById("max").innerHTML += weather.temp_max + " °C";
     // document.getElementById("humidity").innerHTML += weather.humidity + " %";
+
+    const week = [];
+    const dayOne = [];
+    const dayTwo = [];
+    const dayThree = [];
+    const dayFour = [];
+    const dayFive = [];
+
     let current = document.getElementsByClassName("current");
     let max = document.getElementsByClassName("max");
     let humidity = document.getElementsByClassName("humidity");
-    console.log(current);
-    console.log(max);
-    console.log(humidity);
-    // result.innerHTML = response.data.list[0].main.temp_max;
+    for (let i = 0; i < response.data.list.length; i += 8) {
+      console.log(response.data.list[i].main); // shows the data for the 5 days (the +8 is because you have 40elements. 40 divided by 5 days is 8)
+      week.push(response.data.list[i].main); // pushes the 5days items into 1 array
+
+      dayOne.push(week[0]);
+      dayTwo.push(week[1]);
+      dayThree.push(week[2]);
+      dayFour.push(week[3]);
+      dayFive.push(week[4]);
+    }
   });
 }
 getWeather();
