816cba193647e95f9e1a3018fb87e1d4410db151
Kris de Rycke <derycke.kris@gmail.com>
2019-07-02 09:45:12 +0200

    change url to metric system

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ function getWeather() {
   let button = document.getElementById("search");
   button.addEventListener("click", async function(e) {
     let city = document.getElementById("input").value;
-    let link = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${key}&unit=metrics`;
+    let link = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${key}&unit=metrics`;
     let response = await axios.get(link);
     let result = document.getElementById("result");
+    console.log(response.data.list);
     console.log(response.data.list[0].main);
+    console.log(response.data.list[0].main.temp);
+    let weather = response.data.list[0].main;
+
+    document.getElementById("current").innerHTML = weather.temp;
+    document.getElementById("min").innerHTML = weather.temp_min;
+    document.getElementById("max").innerHTML = weather.temp_max;
+    document.getElementById("humidity").innerHTML = weather.humidity;
 
     // result.innerHTML = response.data.list[0].main.temp_max;
   });
