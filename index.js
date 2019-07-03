5a6aeb0635375082d7018ef5ad51f43177fd4754
Kris de Rycke <derycke.kris@gmail.com>
2019-07-03 12:08:47 +0200

    add icons

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function getWeather() {
     let city = document.getElementById("input").value;
     let link = `http://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&APPID=${key}&unit=metrics`;
     let response = await axios.get(link);
+    console.log(response);
 
     //console.log(response.data.list);
     // console.log(response.data.list[0].main);
@@ -25,7 +26,7 @@ function getWeather() {
 
     for (let i = 0; i < response.data.list.length; i += 8) {
       //   console.log(response.data.list[i].main); // shows the data for the 5 days (the +8 is because you have 40elements. 40 divided by 5 days is 8)
-      week.push(response.data.list[i].main); // pushes the 5days weather details into 1 array
+      week.push(response.data.list[i]); // pushes the 5days weather details into 1 array
     }
     dayOne.push(week[0]);
     dayTwo.push(week[1]);
@@ -39,34 +40,28 @@ function getWeather() {
     let current3 = document.getElementById("current3");
     let current4 = document.getElementById("current4");
     let current5 = document.getElementById("current5");
-    let max1 = document.getElementById("max1");
-    let max2 = document.getElementById("max2");
-    let max3 = document.getElementById("max3");
-    let max4 = document.getElementById("max4");
-    let max5 = document.getElementById("max5");
-    let humidity1 = document.getElementById("humidity1");
-    let humidity2 = document.getElementById("humidity2");
-    let humidity3 = document.getElementById("humidity3");
-    let humidity4 = document.getElementById("humidity4");
-    let humidity5 = document.getElementById("humidity5");
+    let wind1 = document.getElementById("wind1");
+    let wind2 = document.getElementById("wind2");
+    let wind3 = document.getElementById("wind3");
+    let wind4 = document.getElementById("wind4");
+    let wind5 = document.getElementById("wind5");
+    // let humidity1 = document.getElementById("humidity1");
+    // let humidity2 = document.getElementById("humidity2");
+    // let humidity3 = document.getElementById("humidity3");
+    // let humidity4 = document.getElementById("humidity4");
+    // let humidity5 = document.getElementById("humidity5");
 
-    current1.innerHTML = dayOne[0].temp + " °C";
-    current2.innerHTML = dayTwo[0].temp + " °C";
-    current3.innerHTML = dayThree[0].temp + " °C";
-    current4.innerHTML = dayFour[0].temp + " °C";
-    current5.innerHTML = dayFive[0].temp + " °C";
+    current1.innerHTML = "  " + dayOne[0].main.temp + " °C";
+    current2.innerHTML = "  " + dayTwo[0].main.temp + " °C";
+    current3.innerHTML = "  " + dayThree[0].main.temp + " °C";
+    current4.innerHTML = "  " + dayFour[0].main.temp + " °C";
+    current5.innerHTML = "  " + dayFive[0].main.temp + " °C";
 
-    max1.innerHTML = dayOne[0].temp_max + " °C";
-    max2.innerHTML = dayTwo[0].temp_max + " °C";
-    max3.innerHTML = dayThree[0].temp_max + " °C";
-    max4.innerHTML = dayFour[0].temp_max + " °C";
-    max5.innerHTML = dayFive[0].temp_max + " °C";
-
-    humidity1.innerHTML = dayOne[0].humidity + " %";
-    humidity2.innerHTML = dayTwo[0].humidity + " %";
-    humidity3.innerHTML = dayThree[0].humidity + " %";
-    humidity4.innerHTML = dayFour[0].humidity + " %";
-    humidity5.innerHTML = dayFive[0].humidity + " %";
+    wind1.innerHTML = "  " + dayOne[0].wind.speed + " Km/h";
+    wind2.innerHTML = "  " + dayTwo[0].wind.speed + " Km/h";
+    wind3.innerHTML = "  " + dayThree[0].wind.speed + " Km/h";
+    wind4.innerHTML = "  " + dayFour[0].wind.speed + " Km/h";
+    wind5.innerHTML = "  " + dayFive[0].wind.speed + " Km/h";
   });
 }
 getWeather();
