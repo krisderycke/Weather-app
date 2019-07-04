be0e1806db826ab23494ab68979d82964bb0ed30
Kris de Rycke <derycke.kris@gmail.com>
2019-07-04 11:47:56 +0200

    change onclick event to keydown in order to use enter

--- index.js
+++ index.js
@@ -21,76 +21,79 @@ for (let d = 0; d < dayOutput.length; d++, day++) {
 }
 
 function getWeather() {
-  let button = document.getElementById("search");
-  button.addEventListener("click", async function(e) {
-    let city = document.getElementById("input").value;
-    let link = `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&APPID=${key}`;
-    let response = await axios.get(link);
-    let backgroundPicture = `https://api.unsplash.com/search/photos?client_id=14ae078181c4ac0c05e7c214aa7f14a9d0351d246345edf147276c0b60ce8cea&page=1&query=${city}`;
-    let responseBg = await axios.get(backgroundPicture);
-    let background =
-      responseBg.data.results[Math.floor(Math.random() * 10)].urls.full;
-    document.body.style.backgroundImage = `url('${background}')`;
-    console.log(background);
-    console.log(response);
+  let input = document.getElementById("input");
 
-    const week = [];
-    const dayOne = [];
-    const dayTwo = [];
-    const dayThree = [];
-    const dayFour = [];
-    const dayFive = [];
+  input.addEventListener("keydown", async function(e) {
+    if (e.keyCode === 13) {
+      let city = document.getElementById("input").value;
+      let link = `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&APPID=${key}`;
+      let response = await axios.get(link);
+      let backgroundPicture = `https://api.unsplash.com/search/photos?client_id=14ae078181c4ac0c05e7c214aa7f14a9d0351d246345edf147276c0b60ce8cea&page=1&query=${city}`;
+      let responseBg = await axios.get(backgroundPicture);
+      let background =
+        responseBg.data.results[Math.floor(Math.random() * 10)].urls.full;
+      document.body.style.backgroundImage = `url('${background}')`;
+      console.log(background);
+      console.log(response);
 
-    for (let i = 0; i < response.data.list.length; i += 8) {
-      //   console.log(response.data.list[i].main); // shows the data for the 5 days (the +8 is because you have 40elements. 40 divided by 5 days is 8)
-      week.push(response.data.list[i]); // pushes the 5days weather details into 1 array
-    }
-    dayOne.push(week[0]);
-    dayTwo.push(week[1]);
-    dayThree.push(week[2]);
-    dayFour.push(week[3]);
-    dayFive.push(week[4]);
-    console.log(week);
+      const week = [];
+      const dayOne = [];
+      const dayTwo = [];
+      const dayThree = [];
+      const dayFour = [];
+      const dayFive = [];
 
-    // let current1 = document.getElementById("current1");
-    // let current2 = document.getElementById("current2");
-    // let current3 = document.getElementById("current3");
-    // let current4 = document.getElementById("current4");
-    // let current5 = document.getElementById("current5");
+      for (let i = 0; i < response.data.list.length; i += 8) {
+        //   console.log(response.data.list[i].main); // shows the data for the 5 days (the +8 is because you have 40elements. 40 divided by 5 days is 8)
+        week.push(response.data.list[i]); // pushes the 5days weather details into 1 array
+      }
+      dayOne.push(week[0]);
+      dayTwo.push(week[1]);
+      dayThree.push(week[2]);
+      dayFour.push(week[3]);
+      dayFive.push(week[4]);
+      console.log(week);
 
-    // current1.innerHTML = "  " + dayOne[0].main.temp_max + " °C";
-    // current2.innerHTML = "  " + dayTwo[0].main.temp_max + " °C";
-    // current3.innerHTML = "  " + dayThree[0].main.temp_max + " °C";
-    // current4.innerHTML = "  " + dayFour[0].main.temp_max + " °C";
-    // current5.innerHTML = "  " + dayFive[0].main.temp_max + " °C";
-    let currents = document.getElementsByClassName("current");
-    for (let i = 0; i < currents.length; i++) {
-      currents[i].innerHTML = "  " + week[i].main.temp_max + " °C";
-    }
+      // let current1 = document.getElementById("current1");
+      // let current2 = document.getElementById("current2");
+      // let current3 = document.getElementById("current3");
+      // let current4 = document.getElementById("current4");
+      // let current5 = document.getElementById("current5");
 
-    // let wind1 = document.getElementById("wind1");
-    // let wind2 = document.getElementById("wind2");
-    // let wind3 = document.getElementById("wind3");
-    // let wind4 = document.getElementById("wind4");
-    // let wind5 = document.getElementById("wind5");
+      // current1.innerHTML = "  " + dayOne[0].main.temp_max + " °C";
+      // current2.innerHTML = "  " + dayTwo[0].main.temp_max + " °C";
+      // current3.innerHTML = "  " + dayThree[0].main.temp_max + " °C";
+      // current4.innerHTML = "  " + dayFour[0].main.temp_max + " °C";
+      // current5.innerHTML = "  " + dayFive[0].main.temp_max + " °C";
+      let currents = document.getElementsByClassName("current");
+      for (let i = 0; i < currents.length; i++) {
+        currents[i].innerHTML = "  " + week[i].main.temp_max + " °C";
+      }
 
-    // wind1.innerHTML = "  " + dayOne[0].wind.speed + " Km/h";
-    // wind2.innerHTML = "  " + dayTwo[0].wind.speed + " Km/h";
-    // wind3.innerHTML = "  " + dayThree[0].wind.speed + " Km/h";
-    // wind4.innerHTML = "  " + dayFour[0].wind.speed + " Km/h";
-    // wind5.innerHTML = "  " + dayFive[0].wind.speed + " Km/h";
-    // console.log(week[i].wind.speed);
-    let winds = document.getElementsByClassName("wind");
-    for (let i = 0; i < winds.length; i++) {
-      winds[i].innerHTML = "  " + week[i].wind.speed + " Km/h";
-    }
+      // let wind1 = document.getElementById("wind1");
+      // let wind2 = document.getElementById("wind2");
+      // let wind3 = document.getElementById("wind3");
+      // let wind4 = document.getElementById("wind4");
+      // let wind5 = document.getElementById("wind5");
 
-    let icons = document.getElementsByClassName("icon");
-    for (let i = 0; i < icons.length; i++) {
-      icons[i].src =
-        "https://openweathermap.org/img/wn/" +
-        week[i].weather[0].icon +
-        "@2x.png";
+      // wind1.innerHTML = "  " + dayOne[0].wind.speed + " Km/h";
+      // wind2.innerHTML = "  " + dayTwo[0].wind.speed + " Km/h";
+      // wind3.innerHTML = "  " + dayThree[0].wind.speed + " Km/h";
+      // wind4.innerHTML = "  " + dayFour[0].wind.speed + " Km/h";
+      // wind5.innerHTML = "  " + dayFive[0].wind.speed + " Km/h";
+      // console.log(week[i].wind.speed);
+      let winds = document.getElementsByClassName("wind");
+      for (let i = 0; i < winds.length; i++) {
+        winds[i].innerHTML = "  " + week[i].wind.speed + " Km/h";
+      }
+
+      let icons = document.getElementsByClassName("icon");
+      for (let i = 0; i < icons.length; i++) {
+        icons[i].src =
+          "https://openweathermap.org/img/wn/" +
+          week[i].weather[0].icon +
+          "@2x.png";
+      }
     }
   });
 }
