f421b3f8390e6c4c22d2646bc43536bb46e0cf93
Kris de Rycke <derycke.kris@gmail.com>
2019-07-05 09:04:45 +0200

    url bug

--- index.js
+++ index.js
@@ -105,44 +105,44 @@ function getWeather() {
         console.log(iconCode);
         switch (iconCode) {
           case "01d":
-            results[i].style.background = "url('/Assets/Pics/01d.jpg')";
+            results[i].style.background = "url('Assets/Pics/01d.jpg')";
             break;
           case "01n":
-            results[i].style.background = "url('/Assets/Pics/01n.jpg')";
+            results[i].style.background = "url('Assets/Pics/01n.jpg')";
             break;
           case "02d":
-            results[i].style.background = "url('/Assets/Pics/02d.jpg')";
+            results[i].style.background = "url('Assets/Pics/02d.jpg')";
             break;
           case "02n":
-            results[i].style.background = "url('/Assets/Pics/02n.jpg')";
+            results[i].style.background = "url('Assets/Pics/02n.jpg')";
             break;
           case "03d":
-            results[i].style.background = "url('/Assets/Pics/03d.jpg')";
+            results[i].style.background = "url('Assets/Pics/03d.jpg')";
             break;
           case "03n":
-            results[i].style.background = "url('./Assets/Pics/02n.jpg')";
+            results[i].style.background = "url('Assets/Pics/02n.jpg')";
             break;
           case "04d":
           case "04n":
-            results[i].style.background = "url('./Assets/Pics/04d.jpg')";
+            results[i].style.background = "url('Assets/Pics/04d.jpg')";
             break;
           case "09d":
           case "10d":
           case "09n":
           case "10n":
-            results[i].style.background = "url('./Assets/Pics/09-10d.jpg')";
+            results[i].style.background = "url('Assets/Pics/09-10d.jpg')";
             break;
           case "11d":
           case "11n":
-            results[i].style.background = "url('./Assets/Pics/11d.jpg')";
+            results[i].style.background = "url('Assets/Pics/11d.jpg')";
             break;
           case "13d":
           case "13n":
-            results[i].style.background = "url('./Assets/Pics/13d.jpg')";
+            results[i].style.background = "url('Assets/Pics/13d.jpg')";
             break;
           case "50d":
           case "50n":
-            results[i].style.background = "url('./Assets/Pics/50d.jpg')";
+            results[i].style.background = "url('Assets/Pics/50d.jpg')";
             break;
         }
       }
